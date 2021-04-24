2edb9d1ed71605218a76f297614dd24454614505
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2021-04-24 18:34:55 +0500

    meal item widget - card with network images

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class CategoryMealsScrean extends StatelessWidget {
   static const String routeName = '/category-meals';
@@ -21,7 +22,7 @@ class CategoryMealsScrean extends StatelessWidget {
       body: Center(
         child: ListView.builder(
           itemBuilder: (context, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(meal: categoryMeals[index]);
           },
           itemCount: categoryMeals.length,
         ),
