6651591b4411ef0be199e68f6e06ff1c63a330e7
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2021-04-24 14:53:44 +0500

    using named routes and passing data using named routes

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
@@ -9,10 +8,9 @@ class CategoryItem extends StatelessWidget {
   const CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(builder: (_) {
-        return CategoryMealsScrean(id, title);
-      }),
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: {'id': id, 'title': title},
     );
   }
 
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScrean extends StatelessWidget {
-  final String id;
-  final String title;
-
-  const CategoryMealsScrean(this.id, this.title);
+  const CategoryMealsScrean();
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final String id = routeArgs['id'];
+    final String title = routeArgs['title'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.title),
+        title: Text(title),
       ),
       body: Center(
         child: Text('The Recipe for the category!'),
